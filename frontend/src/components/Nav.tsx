a85ecaab4baad89e6e17a71717f48a4f069c63a8
Carles Gallardo <carles.gallardo00@gmail.com>
2025-02-05 20:31:23 +0100

    logo v1

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router";
+import useUser from "../lib/userContext/useUser";
 
 interface Props{
   text: string
@@ -13,12 +14,23 @@ function NavButton({text, link}: Props) {
   );
 }
 
+function NavProfile(){
+  const {user} = useUser();
+  return(
+    <div className="">
+      <img src={user?.picture} className="border-2 border-lime-600 rounded-full h-12" />
+    </div>
+  )
+}
+
 export default function Nav() {
   return (
-    <header className="flex h-12 items-center justify-center border-b-2 border-stone-100">
-      <NavButton text="Feed" link="/feed" />
-      <NavButton text="Dashboard" link="/dashboard"/>
-      <NavButton text="Profile" link="/profile"/>
+    <header className="flex h-18 items-center justify-center border-b-2 border-stone-100">
+      <div className="flex h-full">
+        <NavButton text="Feed" link="/feed" />
+        <NavButton text="Dashboard" link="/dashboard"/>
+      </div>
+      <NavProfile/>
     </header>
   );
 }
